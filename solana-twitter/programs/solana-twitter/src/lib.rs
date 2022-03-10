aac01aab86112382293c57bc9640e0e2c3ed8b67
stellaw1 <stella01wang@gmail.com>
2022-03-10 11:27:18 -0800

    setup & implement first instruction

---
+++ solana-twitter/programs/solana-twitter/src/lib.rs
@@ -0,0 +1,77 @@
+use anchor_lang::prelude::*;
+use anchor_lang::solana_program::system_program;
+
+declare_id!("DUudR1UMozQ2o3v2KhZVjNHiNkXFntWoq4ikz4v78Yq8");
+
+#[program]
+pub mod solana_twitter {
+    use super::*;
+    
+    // instruction that initializes a new tweet
+    pub fn send_tweet(ctx: Context<SendTweet>, topic: String, content: String) -> ProgramResult {
+        let tweet: &mut Account<Tweet> = &mut ctx.accounts.tweet;
+        let author: &Signer = &ctx.accounts.author;
+        let clock: Clock = Clock::get().unwrap(); //unwrap gets the return value inside Ok()
+        
+        if topic.chars().count() > 50 {
+            return Err(ErrorCode::TopicTooLong.into())
+        }
+    
+        if content.chars().count() > 280 {
+            return Err(ErrorCode::ContentTooLong.into())
+        }
+        
+        tweet.author = *author.key; // dereference author's public key
+        tweet.timestamp = clock.unix_timestamp;
+        tweet.topic = topic;
+        tweet.content = content;
+
+        Ok(())
+    }
+}
+
+// define context
+#[derive(Accounts)]
+pub struct SendTweet<'info> {
+    #[account(init, payer = author, space = Tweet::LEN)]
+    pub tweet: Account<'info, Tweet>,
+    #[account(mut)]
+    pub author: Signer<'info>,
+    #[account(address = system_program::ID)]
+    pub system_program: AccountInfo<'info>,
+}
+
+// define account structure
+#[account]
+pub struct Tweet {
+    pub author: Pubkey, 
+    pub timestamp: i64,
+    pub topic: String, 
+    pub content: String,
+}
+
+// define account size
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const TIMESTAMP_LENGTH: usize = 8;
+const STRING_LENGTH_PREFIX: usize = 4; // prefix that stores size of string
+const MAX_TOPIC_LENGTH: usize = 50 * 4; // 50 chars
+const MAX_CONTENT_LENGTH: usize = 280 * 4; // 50 chars
+
+// define Tweet structure
+impl Tweet {
+    const LEN: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH
+        + TIMESTAMP_LENGTH
+        + STRING_LENGTH_PREFIX + MAX_TOPIC_LENGTH
+        + STRING_LENGTH_PREFIX + MAX_CONTENT_LENGTH;
+}
+
+// define errors
+#[error]
+pub enum ErrorCode {
+    #[msg("The provided topic should be 50 characters long maximum.")]
+    TopicTooLong,
+    #[msg("The provided content should be 280 characters long maximum.")]
+    ContentTooLong,
+}
